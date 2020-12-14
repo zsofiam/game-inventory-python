7dd322e869d6ee4ffe304d2018742d52e36ebab0
matyasi.zsofia <zsofia.matyasi@gmail.com>
2020-12-14 17:53:48 +0100

    remove_from_inventory(inventory, removed_items) function implemented

--- game_inventory.py
+++ game_inventory.py
@@ -1,9 +1,3 @@
-
-# This is the file where you must work.
-# Write code in the functions (and create new functions) so that they work
-# according to the requirements.
-
-
 def display_inventory(inventory):
     for key, value in inventory.items(): 
         print(f'{key}, {value}')
@@ -18,8 +12,11 @@ def add_to_inventory(inventory, added_items):
 
 
 def remove_from_inventory(inventory, removed_items):
-    """Remove from the inventory dictionary a list of items from removed_items."""
-    pass
+    for element in removed_items:
+        if element in inventory.keys():
+            inventory[element] -= 1
+            if inventory[element] <= 0:
+                del inventory[element]
 
 
 def print_table(inventory, order):
@@ -45,6 +42,6 @@ def export_inventory(inventory, filename):
 
 if __name__ == "__main__":
     inventory = {'rope': 1, 'torch': 6, 'blanket': 3}
-    display_inventory(inventory)
-    add_to_inventory(inventory, ['dagger', 'shield', 'bread', 'torch', 'dagger'])
+    
+    remove_from_inventory(inventory, ['blanket', 'torch', 'rope', 'torch', 'blanket'])
     display_inventory(inventory)
