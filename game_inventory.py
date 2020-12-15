ee9cd28e86ee590ef0d4c9289e259d0ee535ea3c
matyasi.zsofia <zsofia.matyasi@gmail.com>
2020-12-15 09:23:12 +0100

    print_table(inventory, order=None) function implemented

--- game_inventory.py
+++ game_inventory.py
@@ -19,13 +19,22 @@ def remove_from_inventory(inventory, removed_items):
                 del inventory[element]
 
 
-def print_table(inventory, order):
-    """
-    Display the contents of the inventory in an ordered, well-organized table with
-    each column right-aligned.
-    """
-
-    pass
+def print_table(inventory, order=None):
+    print("""-----------------
+item name | count
+-----------------""")
+    if order == 'count, asc' or order == 'count, desc':
+        list_of_tuples = sorted(inventory.items(), key=lambda x: x[1])
+    if order == 'count, asc':
+        for tuple in list_of_tuples: 
+            print(str.ljust(str(tuple[0]), 9), "|", str.rjust(str(tuple[1]), 5))
+    elif order == 'count, desc':
+        for i in range(len(list_of_tuples)-1, -1, -1):
+            print(str.ljust(str(list_of_tuples[i][0]), 9), "|", str.rjust(str(list_of_tuples[i][1]), 5))
+    else:
+        for key, value in inventory.items():
+            print(str.ljust(str(key), 9), "|", str.rjust(str(value), 5))
+    print("""-----------------""")
 
 
 def import_inventory(inventory, filename):
@@ -41,7 +50,9 @@ def export_inventory(inventory, filename):
 
 
 if __name__ == "__main__":
-    inventory = {'rope': 1, 'torch': 6, 'blanket': 3}
-    
-    remove_from_inventory(inventory, ['blanket', 'torch', 'rope', 'torch', 'blanket'])
-    display_inventory(inventory)
+    inventory = {'rope': 1, 'torch': 6, 'blanket': 3, 'gold coin': 2}
+    print_table(inventory, 'count, asc')
+    print("")
+    print_table(inventory)
+    print("")
+    print_table(inventory, 'count, desc')
