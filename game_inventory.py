216370e117c4699ca3335fff5a92798119edc8bd
matyasi.zsofia <zsofia.matyasi@gmail.com>
2020-12-15 12:05:18 +0100

    errors corrected as per game_inventory_test.py file

--- game_inventory.py
+++ game_inventory.py
@@ -1,6 +1,6 @@
 def display_inventory(inventory):
     for key, value in inventory.items(): 
-        print(f'{key}, {value}')
+        print(f'{key}: {value}')
 
 
 def add_to_inventory(inventory, added_items):
@@ -23,25 +23,25 @@ def print_table(inventory, order=None):
     print("""-----------------
 item name | count
 -----------------""")
-    if order == 'count, asc' or order == 'count, desc':
+    if order == 'count,asc' or order == 'count,desc':
         list_of_tuples = sorted(inventory.items(), key=lambda x: x[1])
-    if order == 'count, asc':
+    if order == 'count,asc':
         for tuple in list_of_tuples: 
-            print(str.ljust(str(tuple[0]), 9), "|", str.rjust(str(tuple[1]), 5))
-    elif order == 'count, desc':
+            print(str.rjust(str(tuple[0]), 9), "|", str.rjust(str(tuple[1]), 5))
+    elif order == 'count,desc':
         for i in range(len(list_of_tuples)-1, -1, -1):
-            print(str.ljust(str(list_of_tuples[i][0]), 9), "|", str.rjust(str(list_of_tuples[i][1]), 5))
+            print(str.rjust(str(list_of_tuples[i][0]), 9), "|", str.rjust(str(list_of_tuples[i][1]), 5))
     else:
         for key, value in inventory.items():
-            print(str.ljust(str(key), 9), "|", str.rjust(str(value), 5))
+            print(str.rjust(str(key), 9), "|", str.rjust(str(value), 5))
     print("""-----------------""")
 
 
-def import_inventory(inventory, filename="test_inventory.csv"):
+def import_inventory(inventory, filename="import_inventory.csv"):
     try:
         f = open(filename, "r")
     except FileNotFoundError:
-        print(f'File "{filename}" not found!')
+        print(f"File '{filename}' not found!")
     else:
         content_of_file_as_list = f.read().split(",")
         add_to_inventory(inventory, content_of_file_as_list)
@@ -69,4 +69,4 @@ def export_inventory(inventory, filename="export_inventory.csv"):
 
 if __name__ == "__main__":
     inventory = {'rope': 1, 'torch': 6, 'blanket': 3, 'gold coin': 2}
-    export_inventory(inventory)
+    import_inventory(inventory, "not_exists.csv")
